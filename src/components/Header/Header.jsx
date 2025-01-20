81632ae510d3bdfcff60cd8b21b63be29978d827
AchiniImesha <pachini892@gmail.com>
2025-01-20 12:24:54 +0530

    Update Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="header-contents">
-            <h2>Order your favourite food here....</h2>
+            <h2>Order your favourite food here..</h2>
             <p>Indulge your senses with our exquisite cuisine. Every dish is a symphony of flavors, carefully crafted to delight your palate. From classic favorites to daring new creations, our menu offers something to satisfy every craving.</p>
             <button>View Menu</button>
              
